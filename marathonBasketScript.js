9c942e8db2275f3d45c3d81c425666a861c12689
Nikita Gukovskiy <GN1998@yandex.ru>
2020-08-03 20:46:23 +0300

    v4 not tested

--- marathonBasketScript.js
+++ marathonBasketScript.js
@@ -1,9 +1,8 @@
 // data-mutable-id: MG2_-1350604717 - first quarter , MG3_1215123098 - second quarter, MG4_-514116383 - third quarter, MG5_2051611432 - fourth quarter
 
 let quarter_duration = 10;
-let latency = 90;
-let sensitivity = 0.025;
-let timeToStart = 1.5 * latency;
+let latency = 61;
+let timeToStart = 150;
 // --------- BEEPER -----------
 const audio = `<audio id="mybeep" src="https://www.soundjay.com/button/button-2.mp3" preload="auto"></audio>`;
 document.body.insertAdjacentHTML("beforeend", audio);
@@ -14,28 +13,25 @@ const log = [];
 const signals = [];
 let maxTotal = -1;
 
-function makeSomeNoise(weight, prevTotal, latestTotal) {
+function makeSomeNoise(minPrevTotal, latestTotal, latestTime) {
+  let diff = latestTotal - minPrevTotal;
   console.log(`*********`);
-  console.log(
-    `Weight: ${weight}. Previous total: ${prevTotal}, total now: ${latestTotal}`
-  );
-  signals.push({ weight: weight, total: latestTotal });
+  console.log(`Previous mintotal: ${minPrevTotal}, total now: ${latestTotal}`);
+  console.log(`Diff: ${diff}`);
+  console.log(`Time: ${latestTime}`);
   console.log(`BET ON ${latestTotal}`);
   console.log(`*********`);
+
+  signals.push({
+    total: latestTotal,
+    time: latestTime,
+    diff: diff,
+  });
   audioNode.play();
 }
 
 function isEqual(data1, data2) {
   return JSON.stringify(data1) === JSON.stringify(data2);
-}
-
-function calcWeight(x1, x2, y1, y2) {
-  // x2 > x1
-  // Sometimes x2 can be equal to x1, and the result is going to be Infinity, but that is OK.
-  // if (x2 === x1) return 0;
-  let tan = (y2 - y1) / (x2 - x1);
-  let weight = (10 * tan) / y1;
-  return weight;
 }
 
 function getOdds() {
@@ -54,44 +50,22 @@ function getOdds() {
 }
 
 function checkForPattern(logArray) {
-  let { time: latestTime, points: latestPoints, total: latestTotal } = logArray[
-    logArray.length - 1
-  ];
-  if (latestTime >= (quarter_duration - 4) * 60 || latestTime < timeToStart)
+  let { time: latestTime, total: latestTotal } = logArray[logArray.length - 1];
+  if (latestTime >= (quarter_duration - 4) * 60 || latestTime <= timeToStart)
     return;
 
   let previousTime = latestTime - latency;
-  let previousArray = logArray
+  let previousTotals = logArray
     .filter((elem) => elem.time >= previousTime)
-    .slice(0, -1);
-  let previousTotals = previousArray.map((elem) => elem.total);
+    .slice(0, -1)
+    .map((elem) => elem.total);
+
   if (!previousTotals) return;
-  let previousMaxTotal = Math.max(...previousTotals);
   let previousMinTotal = Math.min(...previousTotals);
-  let previousMinTime =
-    previousArray[previousTotals.lastIndexOf(previousMinTotal)].time;
-  // console.log(
-  //   `DEBUG: previousMaxTotal: ${previousMaxTotal}, previousMinTotal: ${previousMinTotal}, previousMinTime: ${previousMinTime}`
-  // );
-  if (latestTotal < previousMaxTotal) return;
   if (maxTotal > latestTotal) return;
-  let weight = calcWeight(
-    previousMinTime,
-    latestTime,
-    previousMinTotal,
-    latestTotal
-  );
+  if (latestTotal - previousMinTotal <= 4) return;
 
-  // console.log(`------------`);
-  // console.log("checking");
-  // console.log(`weight: ${weight}`);
-  // console.log(
-  //   `curTime: ${latestTime} curPoints: ${latestPoints} curTotal: ${latestTotal}`
-  // );
-  // console.log(`------------`);
-  if (weight < sensitivity) return;
-
-  makeSomeNoise(weight, previousMaxTotal, latestTotal);
+  makeSomeNoise(previousMinTotal, latestTotal, latestTime);
 }
 
 function getData() {
@@ -102,11 +76,9 @@ function getData() {
     .filter((elem) => elem.length > 1);
   let timeArray = resText[resText.length - 1].split(":");
   let timeInSecs = +timeArray[0] * 60 + +timeArray[1]; // time from the beginning
-  let points = resText[0].split(":").reduce((sum, elem) => +sum + +elem); // sum
   let total = getOdds(); // expecting total
   return {
     time: timeInSecs,
-    points: points,
     total: total,
   };
 }
@@ -121,17 +93,16 @@ function fillLog(data) {
     return;
   }
   if (isEqual(data, log[log.length - 1])) return;
-  if (log.length === 30) {
+  if (log.length === 20) {
     log.shift();
   }
   log.push(data);
   checkForPattern(log);
 }
 
-// по дефолту quarter_duration = 10, latency = 90, sensitivity = 0.025, timeToStart = 1.5*latency
+// по дефолту timeToStart = 150, latency = 61, quarter_duration = 10
+// timeToStart =
+// latency =
 // quarter_duration = 12
-// latency = 60
-// sensitivity =
-// timeToStart = latency*2
 
 setInterval(() => fillLog(getData()), 5000);
