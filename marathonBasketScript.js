22107c5a07e54ba9aed7d81af8637d1d9f23c7a6
Nikita Gukovskiy <GN1998@yandex.ru>
2020-12-25 01:52:30 +0300

    some changes

--- marathonBasketScript.js
+++ marathonBasketScript.js
@@ -1,21 +1,17 @@
-// data-mutable-id: MG2_-1350604717 - первая четверть , MG3_1215123098 - вторая четверть, MG4_-514116383 - третья четверть, MG5_2051611432 - четвертая четверть
+// data-mutable-id: MG2_-2012823853 - первая четверть , MG3_552903962 - вторая четверть, MG4_-1176335519 - третья четверть, MG5_1389392296 - четвертая четверть
 
 let quarter_duration = 10;
 let latency = 61;
 let timeBeforeEnd = 2;
-let sensitivityForLowerThan = 4.5;
-let expectedPointsInMinute = 4.6;
 console.log(`Константы:`);
 console.log(`quarter_duration: ${quarter_duration}`);
 console.log(`latency: ${latency}`);
 console.log(`timeBeforeEnd: ${timeBeforeEnd}`);
-console.log(`sensitivityForLowerThan: ${sensitivityForLowerThan}`);
-console.log(`expectedPointsInMinute: ${expectedPointsInMinute}`);
 console.log(`-------------\n`);
 // --------- BEEPER -----------
-const audio = `<audio id="mybeep" src="https://www.soundjay.com/button/button-2.mp3" preload="auto"></audio>`;
-document.body.insertAdjacentHTML("beforeend", audio);
-const audioNode = document.getElementById("mybeep");
+// const audio = `<audio id="mybeep" src="https://www.soundjay.com/button/button-2.mp3" preload="auto"></audio>`;
+// document.body.insertAdjacentHTML("beforeend", audio);
+// const audioNode = document.getElementById("mybeep");
 // ----------------------------
 
 const log = [];
@@ -78,7 +74,7 @@ function checkForPattern(logArray) {
     logArray.length - 1
   ];
   if (
-    pointsToReach < expectedPointsInMinute ||
+    pointsToReach < latestTotal / quarter_duration - 0.3 || // необходимое кол-во очков в минуту чуть меньше текущего тотала
     latestTime >= (quarter_duration - 4) * 60 ||
     latestTime < (quarter_duration - 4 - timeBeforeEnd) * 60
   )
@@ -92,8 +88,7 @@ function checkForPattern(logArray) {
 
   if (!previousTotals) return;
   let previousMinTotal = Math.min(...previousTotals);
-  // if (maxTotal > latestTotal) return;
-  if (latestTotal - previousMinTotal < sensitivityForLowerThan) return;
+  if (latestTotal - previousMinTotal < latestTotal / quarter_duration) return;
   makeSomeNoise(previousMinTotal, latestTotal, latestTime, pointsToReach);
 }
 
@@ -103,15 +98,16 @@ function isEqual(data1, data2) {
 
 function getOdds() {
   let block =
-    document.querySelector("[data-mutable-id=MG2_-1350604717]") ||
-    document.querySelector("[data-mutable-id=MG3_1215123098]") ||
-    document.querySelector("[data-mutable-id=MG4_-514116383]") ||
-    document.querySelector("[data-mutable-id=MG5_2051611432]");
+    document.querySelector('[data-mutable-id=MG2_-2012823853]') ||
+    document.querySelector('[data-mutable-id=MG3_552903962]') ||
+    document.querySelector('[data-mutable-id=MG4_-1176335519]') ||
+    document.querySelector('[data-mutable-id=MG5_1389392296]');
   if (!block) return 0;
+  const mutableId = block.dataset.mutableId;
   let totals = block
     .querySelectorAll("tbody")[1]
     .querySelectorAll(".coeff-value");
-  let total = +totals[totals.length - 2].textContent.trim().slice(1, -1);
+  let total = +totals[0].textContent.trim().slice(1, -1);
   if (total > maxTotal) maxTotal = total;
   if (total < minTotal) minTotal = total;
   return total;
@@ -119,25 +115,20 @@ function getOdds() {
 
 function getData() {
   let resText = document
-    .querySelector(".result-row")
+    .querySelector('.result-row')
     .textContent.trim()
-    .split(" ")
-    .filter((elem) => elem.length > 1);
-  let timeArray = resText[resText.length - 1].split(":");
-  let timeInSecs = +timeArray[0] * 60 + +timeArray[1]; // time from the beginning
-  let total = getOdds(); // expecting total
+    .split(' ')
+    .filter(elem => elem.length > 1);
+  let timeArray = resText[resText.length - 1].split(':');
+  let timeInSecs = +timeArray[0] * 60 + +timeArray[1]; // время с начала четверти
+  let total = getOdds(); // ожидаемый тотал
 
-  let tempArr = document
-    .querySelector(".result-row")
-    .textContent.trim()
-    .split(" ")
-    .filter((elem) => elem.length > 1);
-  let currentPointsString = tempArr[tempArr.length - 2];
-  if (currentPointsString[currentPointsString.length - 1] === ")") {
+  let currentPointsString = resText[resText.length - 2];
+  if (currentPointsString[currentPointsString.length - 1] === ')') {
     currentPointsString = currentPointsString.slice(0, -1);
   }
   let currentPoints = currentPointsString
-    .split(":")
+    .split(':')
     .reduce((sum, elem) => +sum + +elem);
   let pointsToReach =
     ((total - currentPoints) / (quarter_duration * 60 - timeInSecs)) * 60;
@@ -171,11 +162,10 @@ if (Notification.permission !== "granted") {
   Notification.requestPermission();
 }
 
-// по дефолту latency = 61, timeBeforeEnd = 2, quarter_duration = 10, sensitivityForLowerThan = 5.5, expectedPointsInMinute = 4.8
+// по дефолту latency = 61, timeBeforeEnd = 2, quarter_duration = 10, sensitivityForLowerThan = 5.5
 // latency =
 // timeBeforeEnd = 3
 // quarter_duration = 12
 // sensitivityForLowerThan = 7
-// expectedPointsInMinute = 6
 
 setInterval(() => fillLog(getData()), 5000);
